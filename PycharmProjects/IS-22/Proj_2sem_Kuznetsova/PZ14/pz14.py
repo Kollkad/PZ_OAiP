c5e917719239af0f8b0b265b4615549b80ff25db
Kollkad <ku.vy67@gmail.com>
2024-04-06 12:18:25 +0300

    add new pz14

--- PycharmProjects/IS-22/Proj_2sem_Kuznetsova/PZ14/pz14.py
+++ PycharmProjects/IS-22/Proj_2sem_Kuznetsova/PZ14/pz14.py
@@ -3,4 +3,6 @@ import re
 p = re.compile(r"<img.*?>")
 with open('pazzl.html') as file:
   content = file.read()
+  for i in p.findall(content):
+    print(i)
   print('Количество изображений -', len(p.findall(content)))
