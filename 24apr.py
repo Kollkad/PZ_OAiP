7616b3aceb0a569e3b853ebf5a1659b63ea2ff5c
Kollkad <122125118+Kollkad@users.noreply.github.com>
2024-05-10 12:26:13 +0300

    Update 24apr.py

--- 24apr.py
+++ 24apr.py
@@ -21,3 +21,55 @@ print('--')
 print(img2.__dict__)
 print(img2.resize(), img2.__dict__)
 print(img1.title_upper(), img1.__dict__)
+
+#Пара10 мая вот, 16 пз. превый блок до ---- работает. второй нет
+class Student:
+    def __init__(self, name, surname, marks):
+        self.name = name
+        self.surname = surname
+        self.marks = marks
+    def srednee(self):
+        vse = 0
+        for i in self.marks:
+            vse += i
+        sred = vse / len(self.marks)
+        return sred
+    def otlichnik(self, sred):
+        if sred == 5:
+            return('Отличник')
+        else:
+            return('Не отличник')
+
+student1 = Student('Stasy', 'Bread', [2, 2, 3, 4, 4])
+
+print(student1.__dict__)
+print('Среднее по оценкам', student1.srednee())
+print('Отличник?', student1.otlichnik(student1.srednee()))
+print('---------------------------')
+
+class Tier:#дышать, есть, слышать, бегать
+    def __init__(self, name, atmen, essen):
+        self.name = name
+        self.atmen = atmen
+        self.essen = essen
+    def atmen(self):
+        self.atmen = True
+    def essen(self):
+        self.essen = True
+    #def horen(self):
+        #self.horen = True
+    #def laufen(self):
+        #self.laufen = True
+
+class Katze(Tier):
+    def mur(self):
+        print('mur mur mur')
+
+class Hund(Tier):
+    def gav(self):
+        print('gav gav gav')
+
+animal_none = Tier('Not found', False, False)
+print(animal_none.__dict__, animal_none.essen)
+cat = Katze('Lola', False, False)
+print(cat.__dict__, cat.essen, cat.mur)
